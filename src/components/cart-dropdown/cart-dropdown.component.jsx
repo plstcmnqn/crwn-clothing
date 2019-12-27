494f8991951cb22be3429a4f4e15b3a497e6b9fe
plstcmnqn <plstcmnqn@gmail.com>
2019-12-27 11:59:25 -0800

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -22,8 +22,8 @@ const CartDropdown = ({cartItems,history, dispatch}) => (
              <span className='empty-message'>Your cart is empty</span>
         )}    
         </div>
-        <CustomButton onClick={() =>
-            {history.push('/checkout')
+        <CustomButton onClick={() =>{
+            history.push('/checkout');
             dispatch(toggleCartHidden());
             }}>
                 GO TO CHECKOUT</CustomButton>

0610dff5b64d694bf35e6c62d2ee2024554d8fbd
plstcmnqn <plstcmnqn@gmail.com>
2020-01-09 19:32:02 -0800

    adding express server and stripe payment integration

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
 const StripeCheckoutButton =({price})=>{
@@ -8,8 +9,22 @@ const StripeCheckoutButton =({price})=>{
     const publishableKey = 'pk_test_ZZYoxtIQlPuqCVl0mQx7RWq300IJmvZS4Y';
     
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+       axios({
+           url:'payment',
+           method: 'post',
+           data:{
+               amount: priceForStripe,
+               token
+           }
+       }).then(response =>{
+           alert('Payment successful');
+       }).catch(error =>{
+           console.log('Payment error: ', JSON.parse(error))
+           alert(
+               'There was an issue with your payment. Please use the test credit card'
+           );
+       })
+       
 
     }
 
